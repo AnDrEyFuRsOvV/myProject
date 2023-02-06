fdd64c26a185188b7db28dc12a66491d090b52e8
AnDrEyFuRsOv <andreyfursov3097@gmail.com>
2023-02-06 19:44:28 +0200

    add adaptive

--- gulp-projectt/app/js/main.js
+++ gulp-projectt/app/js/main.js
@@ -4,6 +4,17 @@ $(function () {
     $('.dropdown__btn').on('click', function(){
         $('.dopdown__content').toggleClass('disabled');
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.header__nav').toggleClass('active');
+        
+    });
+    
+
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
+    });
 });
 
-
+        
+        
